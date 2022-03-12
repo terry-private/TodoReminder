d7018348966cd6af59f06e81f2c700624dd4b72e
teruhito <ataeteruhito@gmail.com>
2022-03-12 23:32:15 +0900

    add TodoWidget

--- TodoReminder/Views/TodoDetail/TodoDetailView.swift
+++ TodoReminder/Views/TodoDetail/TodoDetailView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 struct TodoDetailView: View {
     let todo: TodoListItem
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Title")) {
+                Text(todo.title)
+            }
+            Section(header: Text("優先度")) {
+                Picker("優先度", selection: Binding.constant(todo.priority)) {
+                    Text("低")
+                        .tag(TodoPriority.low)
+                    Text("中")
+                        .tag(TodoPriority.medium)
+                    Text("高")
+                        .tag(TodoPriority.high)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+
+            Section(header: Text("締め切り")) {
+                DatePicker("", selection: .constant(todo.startDate))
+                    .disabled(true)
+            }
+            Section(header: Text("メモ")) {
+                TextEditor(text: .constant(todo.note))
+                    .foregroundColor(.black)
+                    .frame(height: 200)
+            }
+        }
+        .navigationTitle("詳細")
     }
 }
 
 struct TodoDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        TodoDetailView(todo: TodoListItem(startDate: Date(), note: "test for preview", priority: TodoPriority.high, title: "Test!!"))
+        TodoDetailView(todo: TodoListItem(startDate: Date(), note: "アジェンダを事前に作成しておく", priority: .low, title: "開発MTG"))
     }
 }
