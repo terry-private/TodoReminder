fb598afdb8460508f10183ca9a8add60bdda4019
teruhito <ataeteruhito@gmail.com>
2022-03-12 17:00:49 +0900

    Initial Commit

---
+++ TodoReminder/App/TodoReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoReminderApp.swift
+//  TodoReminder
+//
+//  Created by 若江照仁 on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct TodoReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TodoListView()
+        }
+    }
+}
---
+++ TodoReminder/Views/TodoList/TodoListView.swift
@@ -0,0 +1,21 @@
+//
+//  TodoListView.swift
+//  TodoReminder
+//
+//  Created by 若江照仁 on 2022/03/12.
+//
+
+import SwiftUI
+
+@MainActor
+struct TodoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoListView()
+    }
+}
